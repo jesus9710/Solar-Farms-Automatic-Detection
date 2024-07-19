79c27deb464f01b28daabd72e92797acc4c9f79b
Jesús <jesusferrom97@gmail.com>
2024-07-19 19:43:55 +0200

    Añadido data augmentation. Añadida opción para cargar pesos de backbone. Añadida función para evaluar el AUC IOU

--- Models/src/train_model.py
+++ Models/src/train_model.py
@@ -2,7 +2,7 @@
 
 import torch # Para el manejo de tensores y operaciones en GPU
 import torchvision.transforms as transforms # Para transformaciones de datos
-import segmentation_models_pytorch as smp 
+from sklearn.metrics import auc
 from pathlib import Path # Para manejo de rutas de archivos
 from utils import * # Funciones y variables del módulo utils
 
@@ -20,29 +20,40 @@ device = torch.device(device_str)  # Dispositivo de cómputo: GPU si está dispo
 load_model = True
 
 if load_model:
-    upsample_path = MODEL_DIR / 'upsample_weights.pth'
-    head_path = MODEL_DIR / 'head_weights.pth'
+    backcone_path = MODEL_DIR / 'BB_ASw_GDLv.pth'
+    upsample_path = MODEL_DIR / 'UP_ASw_GDLv.pth'
+    head_path = MODEL_DIR / 'HE_ASw_GDLv.pth'
 else:
+    backcone_path = None
     upsample_path = None
     head_path = None
 
 model = Segmentation_model(model_identifier = "Aerial_SwinB_SI",
+                           backbone_path=backcone_path,
                            upsample_path=upsample_path,
                            head_path=head_path,
                            num_categories = 2,
-                           criterion = 'Dice',
+                           criterion = 'GDLv',
                            device = 'cuda')
 
 model = model.to(device)
 
 # %% Parámetros
 
-batch_size = 60 #  Tamaño del batch (subconjuntos de datos)
+batch_size = 40 #  Tamaño del batch (subconjuntos de datos)
 epochs = 200 # Número de veces que todo el conjunto de datos es pasado por la red
 
 optimizer = torch.optim.Adam(model.parameters()) # Se elige el optimizador Adam (Adaptive Moment Estimation)
-es = EarlyStopping(patience=20)
-transform = transforms.ToTensor() # Secuencia de transformaciones antes de enviarlo al modelo donde se convertirá la imagen a tensor para ser procesado en GPU
+es = EarlyStopping(patience=10, min_delta=0.02)
+
+# Data augmentation
+transform = transforms.Compose([
+    transforms.RandomHorizontalFlip(p=0.3), # Volteo hotizontal
+    transforms.RandomVerticalFlip(p=0.3) # Volteo vertical
+])
+
+# RandomTransform aplica la misma transformación con misma semilla para imágenes y máscaras
+transform = RandomTransform(transform)
 
 # %% Train/Test Dataloaders
 
@@ -55,7 +66,10 @@ train_size = int(train_ratio * len(dataset))
 val_size = int(val_ratio * len(dataset))
 test_size = len(dataset) - train_size - val_size
 
-train_dataset, val_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, val_size, test_size])  # División del conjunto de datos en entrenamiento y prueba
+train_dataset, val_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, val_size, test_size], torch.Generator().manual_seed(42))  # División del conjunto de datos en entrenamiento y prueba
+
+# Para test no es necesario data augmentation
+test_dataset.transform = None
 
 train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=val_size, shuffle=True)
@@ -71,31 +85,68 @@ if train_model:
 
 # %% Predicción
 
+y_hat_train, soft_preds_train, _, target_train = model.predict(train_dataloader)
+y_hat_val, soft_preds_val, _, target_val = model.predict(val_dataloader)
 y_hat, soft_preds, images, target = model.predict(test_dataloader)
 
-# %% Evaluación
+# %% Evaluación (threshold = 0.5)
 
 base_preds = (torch.zeros(*y_hat.shape)).long().to(device) # Baseline (todos los píxeles del valor de la clase mayoritaria)
 
 base_score = evaluate_model(base_preds, target)
+train_score = evaluate_model(y_hat_train, target_train)
+val_score = evaluate_model(y_hat_val, target_val)
 score = evaluate_model(y_hat, target)
 
 print(f'BaseLine score: {base_score}')
-print(f'model score: {score}')
+print(f'model score (train): {train_score}')
+print(f'model score (val): {val_score}')
+print(f'model score (test): {score}')
+
+# %% Evaluación AUC
+
+base_preds = (torch.ones(*soft_preds.shape)).long().to(device) # Baseline (todos los píxeles del valor de la clase mayoritaria)
+
+base_score, _, _ = evaluate_model_auc(base_preds, target)
+train_score, _, _ = evaluate_model_auc(soft_preds_train, target_train)
+val_score, _, _ = evaluate_model_auc(soft_preds_val, target_val)
+score, thresholds, iou_socres = evaluate_model_auc(soft_preds, target)
+
+print(f'BaseLine score: {base_score}')
+print(f'model score (train): {train_score}')
+print(f'model score (val): {val_score}')
+print(f'model score (test): {score}')
+
+show_IOU_curve(thresholds, iou_socres)
 
 # %% Visualización aleatoria
 
+# Índice aleatorio
 ind = np.random.randint(0, len(images))
-check_results(images[ind,:,:,:], y_hat[ind,:,:])
+
+th = 0.8
+
+if th:
+    # Los píxeles solares son únicamente aquellos con th% de probabilidad
+    predictions = torch.where(soft_preds[:,1:2,:,:].squeeze(dim=1) >= th, 1, 0)
+    
+else:
+    # Las predicciones son la clase con mayor probabilidad
+    predictions = y_hat
+
+check_results(images[ind,:,:,:], predictions[ind,:,:], target[ind,:,:])
 
 # %% Guardar modelo
 
 save_model = False
-upsample_fname = 'upsample_weights.pth'
-head_fname = 'head_weights.pth'
+backcone_fname = 'BB_152R_GDLv_1.pth'
+upsample_fname = 'UP_152R_GDLv_1.pth'
+head_fname = 'HE_152R_GDLv_1.pth'
 
 if save_model:
+    backbone_state_dict = model.backbone.state_dict()
     upsample_state_dict = model.upsample.state_dict()
     head_state_dict = model.head.state_dict()
+    torch.save(backbone_state_dict, MODEL_DIR / backcone_fname)
     torch.save(upsample_state_dict, MODEL_DIR / upsample_fname)
     torch.save(head_state_dict, MODEL_DIR / head_fname)
